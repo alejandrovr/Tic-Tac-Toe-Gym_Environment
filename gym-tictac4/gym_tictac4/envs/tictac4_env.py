bbf845b5a5941ee314f4596611523e5156d2887b
alejandrovr <alejandrovarelarial@gmail.com>
2019-07-19 15:32:34 +0200

    better env

--- gym-tictac4/gym_tictac4/envs/tictac4_env.py
+++ gym-tictac4/gym_tictac4/envs/tictac4_env.py
@@ -8,40 +8,49 @@ class TicTac4(gym.Env):
     metadata = {'render.modes': ['human']}
 
     def __init__(self):
-        [os.remove(i) for i in glob("/home/alejandro/virtual_chemist/snapshots/vmdscene*.tga")]
-        self.mol = Molecule('3iej') #this should be a roulette wheel
-        self.mol.filter('chain A')
-        self.mol.filter('not water')
-        self.mol.reps.add('protein',style='Licorice',color='16')
-        self.mol.reps.add('not protein',style='Licorice',color='8')
+        [os.remove(i) for i in glob("/home/alejandro/rl_chemist/snapshots/vmdscene*.tga")]
+        self.mol = Molecule('/home/alejandro/rl_chemist/3iej.pdb') #this should be a roulette wheel
+        self.mol.filter('chain A and resname 599')
+        self.mol.reps.add('all',style='Licorice',color='8')
+        #self.mol.reps.add('protein',style='Licorice',color='16')
         self.mol.view()
+        [self.mol._moveVMD(action='scaleout') for i in range(10)]
         first_caption = self.mol._moveVMD(action='init')
         self.state = first_caption
         self.counter = 0
         self.done = 0
         self.reward = 0
-        self.add = [0, 0]
-
+        self.add = {}
+        self.available_actions = ['rotx','roty','rotz','scalein','scaleout','submit']
+        self.white_history_x = [0.0]
+        self.history = []
     def check(self):
         whites = len(np.where(self.state>.7)[0])
-        print('whites',whites)
         total = self.state.shape[0] * self.state.shape[1]
         coef = (whites / total) * 100
-        self.reward = coef * 0.1 #/ self.counter) * 0.1
+        last_coef = self.white_history_x[-1]
+        hist_reward =  coef - last_coef
+        self.reward =  0.0 #hist_reward
         
         if self.done:
-            self.reward = coef #/ self.counter
+            if 'scalein' in self.history:
+                self.reward = 1.0
+            else:
+                self.reward = -1.0
 
     def step(self, target):
+        target = self.available_actions[target]
+        if target not in self.available_actions:
+            raise
+
         if target=='submit':
             self.done = True
             self.check()
             return [self.state, self.reward, self.done, self.add]
         
-
+        #if not submit, update state and compute reward
         self.state = self.mol._moveVMD(action=target)
         self.counter += 1
-        self.render()
         self.check() #get reward
         return [self.state, self.reward, self.done, self.add]
 
@@ -50,10 +59,4 @@ class TicTac4(gym.Env):
         vhandle = getCurrentViewer()
         vhandle.send("mol delete all") 
         self.__init__()
-        self.counter = 0
-        self.done = 0
-        self.reward = 0
-        return self.state
-
-    def render(self):
-        pass
+        return self.state #first caption
