058b5a76ebcca4236c24db5734fbf1f65a4086f7
alejandrovr <alejandrovarelarial@gmail.com>
2019-08-01 09:14:35 +0200

    nextdih implemented

--- gym-tictac4/gym_tictac4/envs/tictac4_env.py
+++ gym-tictac4/gym_tictac4/envs/tictac4_env.py
@@ -13,37 +13,47 @@ class TicTac4(gym.Env):
         self.mol.filter('chain A and resname 599')
         self.mol.reps.add('all',style='Licorice',color='8')
         #self.mol.reps.add('protein',style='Licorice',color='16')
+        self.get_rot_bonds()
         self.mol.view()
-        [self.mol._moveVMD(action='scaleout') for i in range(10)]
+
+        #TODO: randomize starting point
+        self.available_actions = ['rotx','roty','rotz','scalein','scaleout']
+        [self.mol._moveVMD(action='scaleout') for i in range(20)]
         first_caption = self.mol._moveVMD(action='init')
         self.state = first_caption
         self.counter = 0
         self.done = 0
         self.reward = 0
         self.add = {}
-        self.available_actions = ['rotx','roty','rotz','scalein','scaleout','submit']
-        self.white_history_x = [0.0]
         self.history = []
+
+        _, self.mol.dihedrals = guessAnglesAndDihedrals(molbonds)
+
+        l, r = molecule.get_LR(rb, bonds=molbonds,check_cycle=True)
+        for bond in rotatable_bonds:
+            if bond[0] in r and bond[1] in r:
+                splits[tuple(rb)] += 1
+
+
+
     def check(self):
         whites = len(np.where(self.state>.7)[0])
         total = self.state.shape[0] * self.state.shape[1]
         coef = (whites / total) * 100
-        last_coef = self.white_history_x[-1]
-        hist_reward =  coef - last_coef
-        self.reward =  0.0 #hist_reward
-        
-        if self.done:
-            self.reward = len(self.history) * -1.0
+        self.reward = 0.
+
+        #timeout
+        if self.counter > 30:
+            self.reward = coef * 100.
+            self.done = True        
+            return
+
 
     def step(self, target):
         target = self.available_actions[target]
         if target not in self.available_actions:
             raise
         self.history.append(target)
-        if target=='submit':
-            self.done = True
-            self.check()
-            return [self.state, self.reward, self.done, self.add]
         
         #if not submit, update state and compute reward
         self.state = self.mol._moveVMD(action=target)
