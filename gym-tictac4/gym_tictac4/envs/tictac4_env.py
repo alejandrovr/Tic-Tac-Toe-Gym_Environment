c7e319c054d6abf649130d4c34ff275a7c8ad1fb
alejandrovr <alejandrovarelarial@gmail.com>
2019-09-03 09:10:47 +0200

    summer progress

--- gym-tictac4/gym_tictac4/envs/tictac4_env.py
+++ gym-tictac4/gym_tictac4/envs/tictac4_env.py
@@ -3,23 +3,40 @@ from gym import error, spaces, utils
 from gym.utils import seeding
 from htmd.ui import *
 from htmd.vmdviewer import getCurrentViewer
+import random
+
+def LJ_potential(prot,lig):
+    from scipy.spatial.distance import cdist
+    pcoords = prot.coords.squeeze()
+    ligcoords = lig.coords.squeeze()
+    pl_dists = cdist(pcoords,ligcoords)
+    return np.min(pl_dists)
 
 class TicTac4(gym.Env):
     metadata = {'render.modes': ['human']}
 
     def __init__(self):
         [os.remove(i) for i in glob("/home/alejandro/rl_chemist/snapshots/vmdscene*.tga")]
-        self.mol = Molecule('/home/alejandro/rl_chemist/3iej.pdb') #this should be a roulette wheel
-        self.mol.filter('chain A and resname 599')
-        self.mol.reps.add('all',style='Licorice',color='8')
-        #self.mol.reps.add('protein',style='Licorice',color='16')
-        self.get_rot_bonds()
-        self.mol.view()
+        
+        levels = {'3iej.pdb':('A','599'),}
+        levels_keys = list(levels.keys())
+        selected_level = random.choice(levels_keys)
+        
+        crystal = Molecule(selected_level)
+        prot = crystal.copy()
+        prot.filter('protein and chain {} and noh and (same residue as within 5 of resname {})'.format(levels[selected_level][0],levels[selected_level][1]))
+        self.prot = prot.copy()
+        self.prot.view(style='Licorice',color=8)
 
+        self.mol = crystal.copy()
+        self.mol.filter('chain {} and resname {}'.format(levels[selected_level][0],levels[selected_level][1]))
+        self.mol.get_rot_bonds()
+        print('LJ:',LJ_potential(self.prot,self.mol))
+        self.mol.view()        
+        [self.mol._moveVMD(action='scaleout') for i in range(6)]
         #TODO: randomize starting point
-        self.available_actions = ['rotx','roty','rotz','scalein','scaleout']
-        [self.mol._moveVMD(action='scaleout') for i in range(20)]
-        first_caption = self.mol._moveVMD(action='init')
+        self.available_actions = ['rotx','roty','rotz','switch_dir','movedih','nextdih']
+        first_caption = self.mol._moveVMD(action='nextdih')
         self.state = first_caption
         self.counter = 0
         self.done = 0
@@ -27,26 +44,20 @@ class TicTac4(gym.Env):
         self.add = {}
         self.history = []
 
-        _, self.mol.dihedrals = guessAnglesAndDihedrals(molbonds)
-
-        l, r = molecule.get_LR(rb, bonds=molbonds,check_cycle=True)
-        for bond in rotatable_bonds:
-            if bond[0] in r and bond[1] in r:
-                splits[tuple(rb)] += 1
-
-
-
     def check(self):
-        whites = len(np.where(self.state>.7)[0])
-        total = self.state.shape[0] * self.state.shape[1]
-        coef = (whites / total) * 100
-        self.reward = 0.
-
-        #timeout
-        if self.counter > 30:
-            self.reward = coef * 100.
-            self.done = True        
-            return
+        ljpot = LJ_potential(self.prot,self.mol)
+        if ljpot < 1.0: #reward clashes
+            self.reward = 10.0
+            self.done = True 
+            
+        elif self.counter > 5:
+            self.reward = -10.0
+            self.done = True
+            
+        else:
+            self.reward = 0.0
+            
+        return
 
 
     def step(self, target):
@@ -54,16 +65,18 @@ class TicTac4(gym.Env):
         if target not in self.available_actions:
             raise
         self.history.append(target)
-        
+        print(self.history)
         #if not submit, update state and compute reward
         self.state = self.mol._moveVMD(action=target)
         self.counter += 1
+        #input('next?')
         self.check() #get reward
         return [self.state, self.reward, self.done, self.add]
 
     def reset(self):
         #reset view!
         vhandle = getCurrentViewer()
-        vhandle.send("mol delete all") 
+        #vhandle.send("mol delete all") 
+        vhandle.send("quit") 
         self.__init__()
         return self.state #first caption
